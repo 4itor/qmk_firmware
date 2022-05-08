ee7d44b7e8e22ebb423af8def043a1b683b32703
Aitor <54933598+4itor@users.noreply.github.com>
2022-05-08 22:36:19 +0200

    Reviung: Mouse acceleration

--- keyboards/reviung/reviung34/keymaps/4itor/keymap.c
+++ keyboards/reviung/reviung34/keymaps/4itor/keymap.c
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_MOUS] = LAYOUT_reviung34(
   //,--------------------------------------------.  ,--------------------------------------------.
-  //| (Lock) |        | AltTab-Switcher |  Enter |  |        |        |        |        |        |
+  //| (Lock) |        | AltTab-Switcher |  Enter |  | Accel0 | Accel1 | Accel2 |        |        |
   //|--------|--------|-----------------|--------|  |--------|--------|--------|--------|--------|
   //|   Gui  |   Alt  |  Ctrl  |  Shift | BackSp |  | Mous_L | Mous_D | Mous_U | Mous_R |        |
   //|--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------|
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------|
   //                           |   Tab  | (Held) |  | Mouse1 | Mouse2 |
   //                           `-----------------'  `-----------------'
-      K_LLOCK,  _void_, K_ALTBP, K_ALTAB,  KC_ENT,     _void_,  _void_,  _void_,  _void_,  _void_,
+      K_LLOCK,  _void_, K_ALTBP, K_ALTAB,  KC_ENT,    KC_ACL0, KC_ACL1, KC_ACL2,  _void_,  _void_,
       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_BSPC,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  _void_,
        K_Undo,   K_Cut,  K_Copy, K_Paste,  KC_DEL,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,  _void_,
                                   _void_,  _void_,    KC_BTN1, KC_BTN2
