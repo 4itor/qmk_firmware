c094c75c1a0dcd03404c2c7aac5ae6d7a386e301
Aitor <54933598+4itor@users.noreply.github.com>
2022-06-20 19:22:21 +0200

    Reviung34

--- keyboards/reviung/reviung34/keymaps/4itor/keymap.c
+++ keyboards/reviung/reviung34/keymaps/4itor/keymap.c
@@ -207,7 +207,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //*** Combos!
 
 enum combo_events {
-    COMBO_ESC,   //  U+I+O -> Esc
+    COMBO_ESC1,  //  U+I+O -> Esc
+    COMBO_ESC2,  //  W+E+R -> Esc
     COMBO_CAPSW, //  V+M -> CapsWord
     COMBO_LPRN,  //  R+T -> (
     COMBO_RPRN,  //  Y+U -> )
@@ -221,6 +222,7 @@ enum combo_events {
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
 const uint16_t PROGMEM combo_uio[]    = {KC_U, KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo_wer[]    = {KC_W, KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM combo_vm[]     = {KC_V, KC_M, COMBO_END};
 const uint16_t PROGMEM combo_rt[]     = {KC_R, KC_T, COMBO_END};
 const uint16_t PROGMEM combo_yu[]     = {KC_Y, KC_U, COMBO_END};
@@ -230,7 +232,18 @@ const uint16_t PROGMEM combo_vb[]     = {KC_V, KC_B, COMBO_END};
 const uint16_t PROGMEM combo_nm[]     = {KC_N, KC_M, COMBO_END};
 const uint16_t PROGMEM combo_adj_qp[] = {K_RST_Q, K_RST_P, COMBO_END};
 
-combo_t key_combos[COMBO_LENGTH] = {[COMBO_ESC] = COMBO(combo_uio, KC_ESC), [COMBO_CAPSW] = COMBO_ACTION(combo_vm), [COMBO_LPRN] = COMBO(combo_rt, KC_LPRN), [COMBO_RPRN] = COMBO(combo_yu, KC_RPRN), [COMBO_LCBR] = COMBO(combo_fg, KC_LCBR), [COMBO_RCBR] = COMBO(combo_hj, KC_RCBR), [COMBO_LBRC] = COMBO(combo_vb, KC_LBRC), [COMBO_RBRC] = COMBO(combo_nm, KC_RBRC), [COMBO_RESET] = COMBO(combo_adj_qp, QK_BOOT)};
+combo_t key_combos[COMBO_LENGTH] = {
+    [COMBO_ESC1] = COMBO(combo_uio, KC_ESC),
+    [COMBO_ESC2] = COMBO(combo_wer, KC_ESC),
+    [COMBO_CAPSW] = COMBO_ACTION(combo_vm),
+    [COMBO_LPRN] = COMBO(combo_rt, KC_LPRN),
+    [COMBO_RPRN] = COMBO(combo_yu, KC_RPRN),
+    [COMBO_LCBR] = COMBO(combo_fg, KC_LCBR),
+    [COMBO_RCBR] = COMBO(combo_hj, KC_RCBR),
+    [COMBO_LBRC] = COMBO(combo_vb, KC_LBRC),
+    [COMBO_RBRC] = COMBO(combo_nm, KC_RBRC),
+    [COMBO_RESET] = COMBO(combo_adj_qp, QK_BOOT)
+};
 
 //*** Functions
 
