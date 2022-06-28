564622e1bc506ab797950d78bb13a7fe7fc2c078
Aitor <54933598+4itor@users.noreply.github.com>
2022-06-28 20:00:37 +0200

    Reviung34

--- keyboards/reviung/reviung34/keymaps/4itor/keymap.c
+++ keyboards/reviung/reviung34/keymaps/4itor/keymap.c
@@ -46,18 +46,6 @@ enum custom_keycodes {
 #define ALT_L    LALT_T(KC_L)
 #define GUI_SCLN RGUI_T(KC_SCLN)
 
-// Layer 1 (_NUMB) - Left-hand home row mods
-#define GUI_F5   LGUI_T(KC_F5)
-#define ALT_F6   LALT_T(KC_F6)
-#define CTL_F7   LCTL_T(KC_F7)
-#define SFT_F8   LSFT_T(KC_F8)
-
-// Layer 1 (_NUMB) - Right-hand home row mods
-#define SFT_4    RSFT_T(KC_4)
-#define CTL_5    RCTL_T(KC_5)
-#define ALT_6    LALT_T(KC_6)
-#define GUI_PSL  RGUI_T(KC_PSLS)
-
 // Layer 3 (_NAVI)
 #define K_Quit   LGUI(KC_Q)
 #define K_Close  LGUI(KC_W)
@@ -69,7 +57,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QRTY] = LAYOUT_reviung34(
   // Combos:
-  //  U+I -> Esc
+  //  U+I+O -> Esc
+  //  W+E+R -> Esc
   //  R+T -> (     Y+U -> )
   //  F+G -> {     H+J -> }
   //  V+B -> [     N+M -> ]
@@ -91,17 +80,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NUMB] = LAYOUT_reviung34(
   //,--------------------------------------------.  ,--------------------------------------------.
-  //|   F9   |  F10   |   F11  |   F12  |        |  |    +   |    7   |    8   |    9   |    *   |
+  //|    1   |    2   |    3   |    4   |    5   |  |    6   |    7   |    8   |    9   |    0   |
   //|--------|--------|--------|--------|--------|  |--------|--------|--------|--------|--------|
-  //| F5,Gui | F6,Alt | F7,Ctl | F8,Sft |        |  |    -   |  4,Sft |  5,Ctl |  6,Alt |  /,Gui |
+  //|   Gui  |   Alt  |   Ctl  |  Shift |   F11  |  |   F12  |  Shift |  Ctrl  |   Alt  |   Gui  |
   //|--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------|
-  //|   F1   |   F2   |   F3   |   F4   |        |  |    0   |    1   |    2   |    3   |    =   |
+  //|   F1   |   F2   |   F3   |   F4   |   F5   |  |   F6   |   F7   |   F8   |   F9   |   F10  |
   //|--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------|
   //                           | (Held) | BackSp |  |  Space | ADJUST |
   //                           `-----------------'  `-----------------'
-        KC_F9,  KC_F10,  KC_F11,  KC_F12,  _void_,    KC_PPLS,    KC_7,    KC_8,    KC_9, KC_PAST,
-       GUI_F5,  ALT_F6,  CTL_F7,  SFT_F8,  _void_,    KC_PMNS,   SFT_4,   CTL_5,   ALT_6, GUI_PSL,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,  _void_,       KC_0,    KC_1,    KC_2,    KC_3, KC_PEQL,
+         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+      KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,  KC_F11,     KC_F12, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
                                MO(_ADJT), KC_BSPC,     KC_SPC, MO(_ADJT)
   ),
 
